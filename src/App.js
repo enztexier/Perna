4795feb3506c24a7c2a5634c1b6a6aaf9967037a
enztexier <enzotexier.studio@gmail.com>
2022-04-18 00:06:14 +0200

    bg-color

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import './App.css';
 
 function App() {
 
+  /*
+  // BLEU
+  const COLORS = [
+      {r: 51, g: 153, b: 255},
+      {r: 153, g: 51, b: 255},
+      {r: 0, g: 0, b: 51},
+      {r: 0, g: 176, b: 153},
+      {r: 102, g: 255, b: 255},
+  ];
 
+  /*
+  // MULTI
+  const COLORS = [
+      {r: 51, g: 153, b: 255},
+      {r: 153, g: 51, b: 255},
+      {r: 0, g: 0, b: 51},
+      {r: 255, g: 178, b: 102},
+      {r: 255, g: 153, b: 255},
+  ];
+  */
 
   // ORANGE
   const COLORS = [
