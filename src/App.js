3e9ce968411d09a55fe9ac4076ceffc003df81a3
enztexier <enzotexier.studio@gmail.com>
2022-04-18 00:02:46 +0200

    bg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
+  // ORANGE
+  const COLORS = [
+      {r: 212, g: 96, b: 101},
+      {r: 253, g: 176, b: 87},
+      {r: 235, g: 147, b: 170},
+      {r: 255, g: 178, b: 102},
+      {r: 255, g: 153, b: 255},
+  ];
+
+
 
 }
 
