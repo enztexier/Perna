b2a6d1ed12a29735d2f8a519b6fb6171a2ad0637
enztexier <enzotexier.studio@gmail.com>
2022-04-19 22:25:54 +0200

    start glow

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
   */
 
   // ORANGE
+  /*
   const COLORS = [
       {r: 212, g: 96, b: 101},
       {r: 253, g: 176, b: 87},
@@ -31,7 +32,20 @@ function App() {
       {r: 255, g: 178, b: 102},
       {r: 255, g: 153, b: 255},
   ];
+*/
 
+class Glow {
+      constructor(x, y, radius, rgb) {
+          this.x = x;
+          this.y = y;
+          this.radius = radius;
+          this.rgb = rgb;
+
+          this.vx = Math.random() * 4;
+          this.vy = Math.random() * 4;
+
+          this.sinVal = Math.random();
+      }
 
 
 }
