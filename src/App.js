e69e67c513aa5b2c11643a63eaaa3f597c1b26b1
enztexier <enzotexier.studio@gmail.com>
2022-04-23 08:21:35 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -14,31 +14,20 @@ function App() {
 */
 
   // MULTI
-  const COLORS = [
-      {r: 51, g: 153, b: 255},
-      {r: 153, g: 51, b: 255},
-      {r: 0, g: 0, b: 51},
-      {r: 255, g: 178, b: 102},
-      {r: 255, g: 153, b: 255},
-  ];
-
-
-/*
-  // ORANGE
   const COLORS = [{
-      r: 212,
-      g: 96,
-      b: 101
+      r: 51,
+      g: 153,
+      b: 255
     },
     {
-      r: 253,
-      g: 176,
-      b: 87
+      r: 153,
+      g: 51,
+      b: 255
     },
     {
-      r: 235,
-      g: 147,
-      b: 170
+      r: 0,
+      g: 0,
+      b: 51
     },
     {
       r: 255,
@@ -51,7 +40,37 @@ function App() {
       b: 255
     },
   ];
-*/
+
+
+  /*
+    // ORANGE
+    const COLORS = [{
+        r: 212,
+        g: 96,
+        b: 101
+      },
+      {
+        r: 253,
+        g: 176,
+        b: 87
+      },
+      {
+        r: 235,
+        g: 147,
+        b: 170
+      },
+      {
+        r: 255,
+        g: 178,
+        b: 102
+      },
+      {
+        r: 255,
+        g: 153,
+        b: 255
+      },
+    ];
+  */
   const MathPI = Math.PI * 2;
 
   class Glow {
@@ -172,7 +191,6 @@ function App() {
   }
 
   new Engine();
-
 }
 
 export default App;
---
+++ src/Menu.js
@@ -0,0 +1,36 @@
+import './Menu.css'
+
+function Menu() {
+	return (
+
+    <body>
+    	<header>
+    		<div id="bottomleft">
+    			<span></span>
+    		</div>
+    		<div id="bottomright">
+    			<span></span>
+    		</div>
+    	</header>
+    	<main>
+    		<div class="left"></div>
+    		<div class="middle">
+    	
+    		</div>
+    		<div class="right"></div>
+    	</main>
+    	<footer>
+    		<div id="topleft">
+    			<span></span>
+    		</div>
+    		<div id="topright">
+    			<span></span>
+    		</div>
+    	</footer>
+    </body>
+
+  )
+
+}
+
+export default Menu
