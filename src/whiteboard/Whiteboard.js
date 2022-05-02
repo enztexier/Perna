97d9634224833153184944da249c09034c9874d0
enztexier <103876188+enztexier@users.noreply.github.com>
2022-05-02 16:21:51 +0200

    Update Whiteboard.js

--- src/whiteboard/Whiteboard.js
+++ src/whiteboard/Whiteboard.js
@@ -1,9 +1,13 @@
 import './Whiteboard.css'
 
 function Whiteboard(){
+
+let a = red;
+let b = blue;
+let c = a =! b ? 'r' : 'b'
   return (
     <div class="whiteboard">
-yooo
+This is Menu
     </div>
   )
 }
