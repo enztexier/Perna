a8608e757b5df6002fea83edd9711217798cdc1e
enztexier <enzotexier.studio@gmail.com>
2022-05-19 17:48:51 +0200

    refactoring

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -49,3 +49,50 @@ export class Menu extends React.Component {
 export default Menu
 
 //<img src={require('./logoWhite.png')} />
+
+/*
+function Menu() {
+
+	const [style, setStyle] = useState("whiteboard");
+	const changeStyle = () => {
+		setStyle("whiteboard2");
+	};
+
+	return (
+
+    <body>
+    	<header onClick={() => setStyle((style) => (style === "whiteboardtop" ? "whiteboardtopend" : "whiteboardtop"))}>
+    		<div id="topleft">
+    			<span></span>
+    		</div>
+				<div id="topcenter">
+				</div>
+    		<div id="topright">
+    			<span></span>
+    		</div>
+    	</header>
+    	<main>
+    		<div className="left" categorie="" onClick={() => setStyle((style) => (style === "whiteboardleft" ? "whiteboardleftend" : "whiteboardleft"))}>
+				</div>
+    		<div className="middle" >
+				<div className={style}>
+
+	      </div>
+    		</div>
+    		<div className="right" onClick={() => setStyle((style) => (style === "whiteboardright" ? "whiteboardrightend" : "whiteboardright"))}>
+				</div>
+    	</main>
+    	<footer onClick={() => setStyle((style) => (style === "whiteboard" ? "whiteboard2" : "whiteboard"))}>
+    		<div id="bottomleft">
+    			<span></span>
+    		</div>
+    		<div id="bottomright">
+    			<span></span>
+    		</div>
+    	</footer>
+    </body>
+
+  )
+
+}
+*/
